9b6ba3c6b81ee4d03e95a53d5b468a1affb3fcdf
Erikson Gonçalves Bezerra <erikson.bezerra@ibm.com>
2022-07-18 16:22:47 -0300

    feat: listing names from people table

--- node/src/index.js
+++ node/src/index.js
@@ -1,31 +1,54 @@
 const express = require("express");
-// const mysql = require("mysql2/promise");
+const mysql = require('mysql');
 
 const app = express();
+const config = {
+	host: "db",
+	user: "root",
+	password: "fullcycle",
+	database: "nodedb",
+};
 
-// const connection = await mysql.createConnection({
-// 	host: "db",
-// 	user: "root",
-// 	password: "fullcycle",
-// 	database: "nodedb",
-// });
+const conn = mysql.createConnection(config);
 
-// await connection.execute(
-// 	"CREATE TABLE IF NOT EXISTS people (id INT NOT NULL AUTO_INCREMENT, name VARCHAR(100), PRIMARY KEY(id))",
-// 	() => {
-// 		console.log(">>> Tabela criada com sucesso!!");
-// 	}
-// );
-
-// connection.end();
+conn.query("CREATE TABLE IF NOT EXISTS people (id INT NOT NULL AUTO_INCREMENT, name VARCHAR(100), PRIMARY KEY(id))");
+conn.query("INSERT INTO people (name) VALUES ('Erikson'), ('FullCycle')");
 
 app.get("/", async (_, res) => {
-	// const [rows] = await connection.execute('SELECT * FROM people');
-	// connection.end();
 
-	// console.log('>>> SELECT RESULT ', rows);
+	const people = await new Promise(function(resolve, reject) {
+		
+		conn.query("SELECT * FROM people",
+			function (error, results) {
+				if (error) reject(error);
 
-	res.send("<h1>Hello Docker</h1>");
+				resolve(results);
+			}
+		);
+	});
+
+	const out = `
+		<h1>Full Cycle Rocks!</h1>
+
+		<table>
+			<thead>
+				<tr>
+					<th align="left" width="40px">ID</th>
+					<th align="left">Nome</th>
+				</tr>
+			</thead>
+			<tbody>
+				${people.map((person) => `
+					<tr>
+						<td align="left">${person.id}</td>
+						<td align="left">${person.name}</td>
+					</tr>
+				`).join('')}
+			</tbody>
+		</table>
+	`;	
+
+	res.send(out);
 });
 
 app.listen("3000", () => {
